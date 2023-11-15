77ff76698bde99096dafc175e2f28e65dddf2c57
Abdullah Alqahtani <0xqahta0@gmail.com>
2023-11-16 00:50:51 +0300

    feat: add query param for choosing the language (ar & en)

--- functions/alerts/get_alert.py
+++ functions/alerts/get_alert.py
@@ -1,18 +1,27 @@
 import os
 import json
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 from bson import json_util, ObjectId
 
 
 def lambda_handler(event, context):
     try:
         query_params = event.get('queryStringParameters', {})
+        lang = query_params.get('lang', 'en')
 
         if 'id' in query_params:
             alert_id = query_params['id']
-            alert = get_alert_by_id(alert_id)
+            try:
+                object_id = ObjectId(alert_id)
+                alert = get_alert_by_id(object_id, lang)
+            except errors.InvalidId:
+                return {
+                    'statusCode': 400,
+                    'headers': {'Content-Type': 'application/json'},
+                    'body': json.dumps({'error': 'Invalid alert ID format'})
+                }
         else:
-            alert = get_alert_by_query(query_params)
+            alert = get_alert_by_query(query_params, lang)
 
         if alert:
             return {
@@ -40,27 +49,26 @@ def lambda_handler(event, context):
         }
 
 
-def get_alert_by_id(alert_id):
+def get_alert_by_id(alert_id, lang):
     try:
         mongodb_uri = os.getenv('MONGO_URI')
         client = MongoClient(mongodb_uri, serverSelectionTimeoutMS=5000)
         db = client['alerts_database']
-        collection = db['alerts']
+        collection = db['alerts_ar'] if lang == 'ar' else db['alerts']
 
-        object_id = ObjectId(alert_id)
-        return collection.find_one({'_id': object_id})
+        return collection.find_one({'_id': alert_id})
     except Exception as e:
         raise Exception(f"Failed to retrieve alert by ID: {str(e)}")
 
 
-def get_alert_by_query(query_params):
+def get_alert_by_query(query_params, lang):
     try:
         mongodb_uri = os.getenv('MONGO_URI')
         client = MongoClient(mongodb_uri, serverSelectionTimeoutMS=5000)
         db = client['alerts_database']
-        collection = db['alerts']
+        collection = db['alerts_ar'] if lang == 'ar' else db['alerts']
         query = {k: v for k, v in query_params.items() if v}
-        alerts = collection.find(query)
-        return list(alerts)
+
+        return list(collection.find(query))
     except Exception as e:
         raise Exception(f"Failed to retrieve alerts by query: {str(e)}")
